7b42197804030a049fb63cd935c28117d3d75539
Pedro André <pedro.mda@gmail.com>
2024-12-08 21:13:06 +0000

    test service

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,19 +1,30 @@
 import { UserInterface } from '../types/user.interface';
 import { UsersService } from './users.service';
 import { TestBed } from '@angular/core/testing';
-
+import { UtilsService } from './utils.service';
 describe('UsersService', () => {
   let usersService: UsersService;
 
+  // Create a mocked version of UtilsService with a partial mock for `pluck`
+  let utilsServiceMock: jest.Mocked<Partial<UtilsService>> = {
+    range: jest.fn(),
+    pluck: jest.fn(),
+  };
+
   beforeEach(() => {
+    // Configure the testing module and inject the dependencies
     TestBed.configureTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UtilsService, useValue: utilsServiceMock }, // Mocked UtilsService
+      ],
     });
 
+    // Inject the UsersService
     usersService = TestBed.inject(UsersService);
   });
 
-  it('creates a service', () => {
+  it('should create the service', () => {
     expect(usersService).toBeTruthy();
   });
 
@@ -33,6 +44,19 @@ describe('UsersService', () => {
       usersService.users = [{ id: '3', name: 'foo' }];
       usersService.removeUser('3');
       expect(usersService.users).toEqual([]);
+    });
+  });
+
+  describe('getUsernames', () => {
+    it('should get usernames', () => {
+      // Mock the `pluck` method to return the usernames
+      utilsServiceMock.pluck = jest.fn().mockReturnValue(['foo']);
+
+      // Call the method and verify the output
+      expect(usersService.getUsernames()).toEqual(['foo']);
+
+      // Verify that `pluck` was called as expected
+      expect(utilsServiceMock.pluck).toHaveBeenCalled();
     });
   });
 });
