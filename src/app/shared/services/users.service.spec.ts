0e113bbccfec03486d2b9aeff20607543e7e83a7
Pedro André <pedro.mda@gmail.com>
2024-12-17 22:34:22 +0000

    new components

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,26 +1,15 @@
 import { UserInterface } from '../types/user.interface';
 import { UsersService } from './users.service';
 import { TestBed } from '@angular/core/testing';
-import { UtilsService } from './utils.service';
+
 describe('UsersService', () => {
   let usersService: UsersService;
 
-  // Create a mocked version of UtilsService with a partial mock for `pluck`
-  let utilsServiceMock: jest.Mocked<Partial<UtilsService>> = {
-    range: jest.fn(),
-    pluck: jest.fn(),
-  };
-
   beforeEach(() => {
-    // Configure the testing module and inject the dependencies
     TestBed.configureTestingModule({
-      providers: [
-        UsersService,
-        { provide: UtilsService, useValue: utilsServiceMock }, // Mocked UtilsService
-      ],
+      providers: [UsersService],
     });
 
-    // Inject the UsersService
     usersService = TestBed.inject(UsersService);
   });
 
@@ -35,28 +24,17 @@ describe('UsersService', () => {
         name: 'foo',
       };
       usersService.addUser(user);
-      expect(usersService.users).toEqual([{ id: '3', name: 'foo' }]);
+      expect(usersService.users$.getValue()).toEqual([
+        { id: '3', name: 'foo' },
+      ]);
     });
   });
 
   describe('removeUser', () => {
     it('should remove a user', () => {
-      usersService.users = [{ id: '3', name: 'foo' }];
+      usersService.users$.next([{ id: '3', name: 'foo' }]);
       usersService.removeUser('3');
-      expect(usersService.users).toEqual([]);
-    });
-  });
-
-  describe('getUsernames', () => {
-    it('should get usernames', () => {
-      // Mock the `pluck` method to return the usernames
-      utilsServiceMock.pluck = jest.fn().mockReturnValue(['foo']);
-
-      // Call the method and verify the output
-      expect(usersService.getUsernames()).toEqual(['foo']);
-
-      // Verify that `pluck` was called as expected
-      expect(utilsServiceMock.pluck).toHaveBeenCalled();
+      expect(usersService.users$.getValue()).toEqual([]);
     });
   });
 });
--- src/app/shared/utils/utils.spec.ts
+++ src/app/shared/utils/utils.spec.ts
@@ -2,17 +2,17 @@ import { pluck, range } from './utils';
 
 describe('utils', () => {
   describe('range', () => {
-    it('return correct range from 1 to 5 ', () => {
+    it('returns correct range from 1 to 5', () => {
       expect(range(1, 5)).toEqual([1, 2, 3, 4]);
     });
 
-    it('return correct range from 41 to 44 ', () => {
+    it('returns correct range from 41 to 44', () => {
       expect(range(41, 44)).toEqual([41, 42, 43]);
     });
   });
 
   describe('pluck', () => {
-    it('return correct result', () => {
+    it('returns correct result', () => {
       const data = [
         { id: '1', name: 'foo' },
         { id: '2', name: 'bar' },
