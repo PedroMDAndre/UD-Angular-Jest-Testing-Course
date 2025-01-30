6013f753ee8abbd3b9d4ba23acdfab45c74f81ba
Pedro André <pedro.mda@gmail.com>
2025-01-30 20:08:05 +0000

    NgRx / e2e testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
+import { PostsComponent } from './posts/posts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TodosComponent],
+  imports: [CommonModule, RouterOutlet, TodosComponent, PostsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
